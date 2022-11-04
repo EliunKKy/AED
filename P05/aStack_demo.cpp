75f9baec916179409bd8418c098fd568e1a81143
Rodrigo Azevedo <rodrigoazevedo337.ra@gmail.com>
2022-11-04 15:56:45 +0000

    Update

--- P05/aStack_demo.cpp
+++ P05/aStack_demo.cpp
@@ -14,7 +14,52 @@ using std::endl;
 int check_parenthesis(const char *s)
 {
   // put your code here (20 lines of code should be enough)
-  return -1;
+  aStack<int> st(100);
+  int x;
+  int flag = 0;
+  for(int i = 0; s[i] != '\0'; i++) // go through input string
+  {
+    if (s[i] == '(')
+    {
+      st.push(i);
+      flag = 1;
+    } 
+    else if (s[i] == ')')
+    {
+      if (!st.is_empty())   // check the stack to prevent errors
+      {
+        x = st.pop();
+        printf("'(' at position %d and matching ')' at position %d\n", x, i);
+      }
+      else
+      {
+        printf("')' unmatched at position %d\n", i);
+        return -1;
+      }
+      flag = 1;
+    }
+  }
+
+  // final conditioning
+  if(flag == 0)
+  {
+    printf("No pareteses!\n");
+    return 0;
+  }
+
+  if(st.is_empty())
+  {
+    return 0;
+  } 
+  else
+  {
+    while(!st.is_empty())
+    {
+      x = st.pop();
+      printf("'(' unmatched at position %d\n", x);
+    }
+    return -1;
+  }
 }
 
 int main(int argc,char **argv)
@@ -29,9 +74,9 @@ int main(int argc,char **argv)
   {
     cout << argv[i] << endl;
     if(check_parenthesis(argv[i]) == 0)
-      cout << "  good" << endl;
+      cout << "  good\n" << endl;
     else
-      cout << "  bad" << endl;
+      cout << "  bad\n" << endl;
   }
   return 0;
 }
